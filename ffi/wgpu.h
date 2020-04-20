5ea988510723fc3aff50a1ddd3b1415f71f103f5
Elijah Insua <tmpvar@gmail.com>
2020-04-20 12:17:09 -0700

    Remove license from source headers
    > This block was needed for vendoring in mozilla-central. As the code is
    > now moved out from wgpu to wgpu-native, it's no longer vendored, so we
    > can remove the nasty headers.
    
    fixes #2

--- ffi/wgpu.h
+++ ffi/wgpu.h
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 /* Generated with cbindgen:0.14.1 */
 
 /* DO NOT MODIFY THIS MANUALLY! This file was generated using cbindgen.
