99570147f5ed840dccac17d0779c9a06d13b7b34
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-06-01 20:12:23 -0400

    Add Device/Limit Accessors and Anisotropy Extension

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,87 @@ lazy_static::lazy_static! {
     static ref GLOBAL: Arc<Global> = Arc::new(Global::new("wgpu", wgc::hub::IdentityManagerFactory));
 }
 
+/// Follow a chain of next pointers and automatically resolve them to the underlying structs.
+///
+/// # Syntax:
+///
+/// Given:
+///
+/// `fn map_thing_descriptor(base: &ThingDescriptor, ext1: Option<&ThingDescriptorExtension1>) -> wgt::ThingDescriptor`
+///
+/// Use the syntax:
+///
+/// `follow_chain!(map_thing_descriptor(base_c_descriptor, ThingDescriptorExtension1STypeValue => ThingDescriptorExtension1))`
+///
+/// # Safety
+///
+/// This macro does not use any internal unsafe blocks. The caller (or most likely the function) needs
+/// to be unsafe. The following constraints must be upheld for this to be valid:
+///
+/// - All pointers in the chain of next pointers must point to either null or a valid extension object
+/// - All structures used as extension objects must be `#[repr(C)]`.
+/// - All structures used as extension objects must have `pub next_in_chain: Option<&ChainedStruct>` and `pub s_type: SType`
+///   as the first and second members respectively.
+///
+/// The result of these rules, and the fact that wgpu-native functions using it do not validate all these assumptions,
+/// using this macro is an indication that the function itself must be made unsafe.
+///
+/// # Notes
+///
+/// Given two or more extension structs of the same SType in the same chain, this macro will favor the latter most. There should
+/// not be more than one extension struct with the same SType in a chain anyway, so this behavior should be unproblematic.
+#[macro_export]
+macro_rules! follow_chain {
+    ($func:ident($base:expr $(, $stype:ident => $ty:ty)*)) => {{
+    #[allow(non_snake_case)] // We use the type name as an easily usable temporary name
+    {
+        $(
+            let mut $stype: Option<&$ty> = None;
+        )*
+        let mut chain_opt: Option<&$crate::ChainedStruct> = $base.next_in_chain;
+        while let Some(next_in_chain) = chain_opt {
+            match next_in_chain.s_type {
+                $(
+                    $crate::SType::$stype => {
+                        let next_in_chain_ptr = next_in_chain as *const $crate::ChainedStruct;
+                        assert_eq!(
+                            0,
+                            next_in_chain_ptr.align_offset(::std::mem::align_of::<$ty>()),
+                            concat!("Chain structure pointer is not aligned correctly to dereference as ", stringify!($ty), ". Correct alignment: {}"),
+                            ::std::mem::align_of::<$ty>()
+                        );
+                        let type_ptr: *const $ty = next_in_chain_ptr as _;
+                        $stype = Some(&*type_ptr);
+                    }
+                )*
+                _ => {}
+            }
+            chain_opt = next_in_chain.next;
+        }
+        $func($base, $($stype),*)
+    }}};
+}
+
+#[repr(u32)]
+pub enum SType {
+    Invalid = 0x00_00_00_00,
+    SurfaceDescriptorFromMetalLayer = 0x00_00_00_01,
+    SurfaceDescriptorFromWindowsHWND = 0x00_00_00_02,
+    SurfaceDescriptorFromXlib = 0x00_00_00_03,
+    SurfaceDescriptorFromHTMLCanvasId = 0x00_00_00_04,
+    ShaderModuleSPIRVDescriptor = 0x00_00_00_05,
+    ShaderModuleWGSLDescriptor = 0x00_00_00_06,
+    /// Placeholder value until real value can be determined
+    AnisotropicFiltering = 0x10_00_00_00,
+    Force32 = 0x7F_FF_FF_FF,
+}
+
+#[repr(C)]
+pub struct ChainedStruct<'c> {
+    next: Option<&'c ChainedStruct<'c>>,
+    s_type: SType,
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn wgpu_get_version() -> std::os::raw::c_uint {
     let major: u32 = env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap();
