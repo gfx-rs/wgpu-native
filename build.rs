f4cba4f6c14131b5027f3a68ed800fabf7ce60ea
rajveermalviya <rajveer0malviya@gmail.com>
2022-06-24 18:14:37 +0530

    add `cfg_aliases`, copied from wgpu-core

--- build.rs
+++ build.rs
@@ -3,6 +3,27 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    // Setup cfg aliases
+    cfg_aliases::cfg_aliases! {
+        // Vendors/systems
+        wasm: { target_arch = "wasm32" },
+        apple: { any(target_os = "ios", target_os = "macos") },
+        unix_wo_apple: {all(unix, not(apple))},
+
+        // Backends
+        vulkan: { all(not(wasm), any(windows, unix_wo_apple, feature = "vulkan-portability")) },
+        metal: { all(not(wasm), apple) },
+        dx12: { all(not(wasm), windows) },
+        dx11: { all(not(wasm), windows) },
+        gl: {
+            any(
+                unix_wo_apple,
+                feature = "angle",
+                wasm
+            )
+        },
+    }
+
     println!("cargo:rerun-if-changed=ffi/webgpu-headers/webgpu.h");
     println!("cargo:rerun-if-changed=ffi/wgpu.h");
 
