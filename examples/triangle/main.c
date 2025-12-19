9951bc665e2f92884f5fdcc24981c2fdfb1ab519
Alex <jalexcole@gmail.com>
2025-12-19 15:01:13 -0600

    Examples pass make .PHONY

--- examples/triangle/main.c
+++ examples/triangle/main.c
@@ -296,8 +296,8 @@ int main(int argc, char *argv[]) {
       }
       continue;
     }
-    case WGPUSurfaceGetCurrentTextureStatus_OutOfMemory:
-    case WGPUSurfaceGetCurrentTextureStatus_DeviceLost:
+    // case WGPUSurfaceGetCurrentTextureStatus_OutOfMemory:
+    // case WGPUSurfaceGetCurrentTextureStatus_DeviceLost:
     case WGPUSurfaceGetCurrentTextureStatus_Force32:
       // Fatal error
       printf(LOG_PREFIX " get_current_texture status=%#.8x\n",
