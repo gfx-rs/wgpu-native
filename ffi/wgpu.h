bbaf852064f531b8849c12550239f0225bee3e9c
James Benton <james.benton2@gmail.com>
2025-12-21 11:46:45 +0000

    Add maxBindingArrayElementsPerShaderStage to wgpu.h

--- ffi/wgpu.h
+++ ffi/wgpu.h
@@ -161,6 +161,7 @@ typedef struct WGPUNativeLimits {
     WGPUChainedStructOut chain;
     uint32_t maxPushConstantSize;
     uint32_t maxNonSamplerBindings;
+    uint32_t maxBindingArrayElementsPerShaderStage;
 } WGPUNativeLimits;
 
 typedef struct WGPUPushConstantRange {
