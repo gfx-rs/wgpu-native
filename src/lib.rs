1487faea32e3b7fc891fd671323582b487038aea
Jeff Crouse <jeff@seethroughlab.com>
2026-03-19 12:56:58 -0400

    metal: expose native device/queue/texture interop handles (#557)
    Co-authored-by: Connor Fitzgerald <connorwadefitzgerald@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use conv::{
     map_query_set_index, map_shader_module, map_surface, map_surface_configuration,
     CreateSurfaceParams,
 };
+#[cfg(all(any(target_os = "ios", target_os = "macos"), feature = "metal"))]
+use foreign_types_shared::ForeignType as _;
 use parking_lot::Mutex;
 use smallvec::SmallVec;
 use std::{
@@ -2538,6 +2540,25 @@ pub unsafe extern "C" fn wgpuDeviceGetQueue(device: native::WGPUDevice) -> nativ
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn wgpuDeviceGetNativeMetalDevice(device: native::WGPUDevice) -> *mut c_void {
+    #[cfg(all(any(target_os = "ios", target_os = "macos"), feature = "metal"))]
+    {
+        let device = device.as_ref().expect("invalid device");
+        let hal_device = device.context.device_as_hal::<hal::api::Metal>(device.id);
+        if let Some(hal_device) = hal_device {
+            let raw_device = hal_device.raw_device().lock();
+            return raw_device.as_ptr().cast();
+        }
+        std::ptr::null_mut()
+    }
+    #[cfg(not(all(any(target_os = "ios", target_os = "macos"), feature = "metal")))]
+    {
+        let _ = device;
+        std::ptr::null_mut()
+    }
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn wgpuDeviceHasFeature(
     device: native::WGPUDevice,
     feature: native::WGPUFeatureName,
@@ -2945,6 +2966,30 @@ pub unsafe extern "C" fn wgpuQueueGetTimestampPeriod(queue: native::WGPUQueue) -
     };
 
     context.queue_get_timestamp_period(queue_id)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wgpuQueueGetNativeMetalCommandQueue(
+    queue: native::WGPUQueue,
+) -> *mut c_void {
+    #[cfg(all(any(target_os = "ios", target_os = "macos"), feature = "metal"))]
+    {
+        let queue = queue.as_ref().expect("invalid queue");
+        let hal_queue = queue
+            .queue
+            .context
+            .queue_as_hal::<hal::api::Metal>(queue.queue.id);
+        if let Some(hal_queue) = hal_queue {
+            let raw_queue = hal_queue.as_raw().lock();
+            return raw_queue.as_ptr().cast();
+        }
+        std::ptr::null_mut()
+    }
+    #[cfg(not(all(any(target_os = "ios", target_os = "macos"), feature = "metal")))]
+    {
+        let _ = queue;
+        std::ptr::null_mut()
+    }
 }
 
 #[no_mangle]
@@ -4215,6 +4260,28 @@ pub unsafe extern "C" fn wgpuTextureGetUsage(
 pub unsafe extern "C" fn wgpuTextureGetWidth(texture: native::WGPUTexture) -> u32 {
     let texture = texture.as_ref().expect("invalid texture");
     texture.data.size.width
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wgpuTextureGetNativeMetalTexture(
+    texture: native::WGPUTexture,
+) -> *mut c_void {
+    #[cfg(all(any(target_os = "ios", target_os = "macos"), feature = "metal"))]
+    {
+        let texture = texture.as_ref().expect("invalid texture");
+        let hal_texture = texture
+            .context
+            .texture_as_hal::<hal::api::Metal>(texture.id);
+        if let Some(hal_texture) = hal_texture {
+            return hal_texture.raw_handle().as_ptr().cast();
+        }
+        std::ptr::null_mut()
+    }
+    #[cfg(not(all(any(target_os = "ios", target_os = "macos"), feature = "metal")))]
+    {
+        let _ = texture;
+        std::ptr::null_mut()
+    }
 }
 
 #[no_mangle]
